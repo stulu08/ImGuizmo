85a121473b58aa99be9d18bfb26c89443c3f17cf
Stulu <derstulu@gmail.com>
2021-11-28 17:51:33 +0100

    Static Runtime support for SEngine

---
+++ premake5.lua
@@ -0,0 +1,42 @@
+project "ImGuizmo"
+	kind "StaticLib"
+    language "C++"
+	if(staticRuntime) then
+		staticruntime "on"
+	end
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"ImGuizmo.h",
+		"ImGuizmo.cpp",
+		"ImCurveEdit.cpp",
+		"ImCurveEdit.h",
+		"ImGradient.cpp",
+		"ImGradient.h",
+		"ImSequencer.h",
+		"ImSequencer.cpp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
